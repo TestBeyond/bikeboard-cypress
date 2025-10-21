570671ce9d54e68a64e6da40f7d39459f7140730
Fernando Papito <papitodev@proton.me>
2025-10-21 15:45:40 -0300

    rollback de teste

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: process.env.BASE_URL,
+    baseUrl: 'http://localhost:3000',
     viewportWidth: 1920,
     viewportHeight: 1080,
     screenshotOnRunFailure: false,
